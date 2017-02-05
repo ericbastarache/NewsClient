67aa4a2a68a4615bd3ec3fb7795df97a9dbf828a
ericb <eric_bastarache@hotmail.com>
2017-02-05 01:29:31 -0500

    Electron entry point

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path');
+const url = require('url');
+
+require('dotenv').config();
+
+let win = null;
+
+app.on('ready', () => {
+  win = new BrowserWindow({width: 1000, height: 800});
+
+  if(process.env.PACKAGE === 'true') {
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'dist/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }));
+  } else {
+    win.loadURL(process.env.HOST);
+    win.webContents.openDevTools();
+  }
+
+  win.on('closed', () => {
+    win = null;
+  });
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
