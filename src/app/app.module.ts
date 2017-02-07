e3b7ca05dfa26397bb2605219da2c9cfa5b31b19
ericb <eric_bastarache@hotmail.com>
2017-02-06 23:59:43 -0500

    Added navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NewsNavbarComponent } from './news-navbar/news-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsNavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/news-navbar/news-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-navbar',
+  templateUrl: './news-navbar.component.html',
+  styleUrls: ['./news-navbar.component.css']
+})
+export class NewsNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
