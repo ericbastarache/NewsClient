28ef5665365e8f94c7170fc2054199db08f4f22d
ericb <eric_bastarache@hotmail.com>
2017-02-15 14:17:30 -0500

    Preliminary get all news function in place (WIP)

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class NewsService {
-  private baseUrl: string = 'https://newsapi.org/';
-  
+  private baseUrl: string = 'https://newsapi.org/v1';
+
   constructor(private http: Http) { }
 
-
+  /*getAll() {
+    let allNews = this.http
+      .get(`${this.baseUrl}/sources?lang=en`)
+      .map('nothing');
+    return allNews;
+  }*/
 }
