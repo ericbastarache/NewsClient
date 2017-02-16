c80582d0a82598138eb8f4480061b747badae930
ericb <eric_bastarache@hotmail.com>
2017-02-15 22:46:49 -0500

    Added article list component

---
+++ src/app/article-list/article-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-list',
+  templateUrl: './article-list.component.html',
+  styleUrls: []
+})
+export class ArticleListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
