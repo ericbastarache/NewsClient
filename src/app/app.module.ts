2f215bf3515dc38ab98d84e817d5deb882b5edbf
ericb <eric_bastarache@hotmail.com>
2017-02-15 22:46:22 -0500

    Added app routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,38 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NewsListComponent } from './news-list/news-list.component';
 
 import { NewsService } from './services/news.service';
 import { NewsNavbarComponent } from './news-navbar/news-navbar.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleItemComponent } from './article-item/article-item.component';
 
+const appRoutes: Routes = [
+  { path: 'article-list', component: ArticleListComponent },
+  { path: 'article-item', component: ArticleItemComponent },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsListComponent,
-    NewsNavbarComponent
+    NewsNavbarComponent,
+    ArticleListComponent,
+    ArticleItemComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [NewsService],
   bootstrap: [AppComponent]
