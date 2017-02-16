324068993cfddbb1deb870be1d63c203bc18346d
ericb <eric_bastarache@hotmail.com>
2017-02-15 20:24:29 -0500

    Updated project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
 import { AppComponent } from './app.component';
 import { NewsListComponent } from './news-list/news-list.component';
 
-import { NewsService } from './news.service';
+import { NewsService } from './services/news.service';
 import { NewsNavbarComponent } from './news-navbar/news-navbar.component';
+
 
 @NgModule({
   declarations: [
