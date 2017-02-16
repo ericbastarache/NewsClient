8ead56982c84ef652647492223f46ef61f359c23
ericb <eric_bastarache@hotmail.com>
2017-02-15 21:21:01 -0500

    Fixed ts lint errors

--- src/app/services/news.service.spec.ts
+++ src/app/services/news.service.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-
+import {} from 'jasmine';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { NewsService } from './news.service';
 
