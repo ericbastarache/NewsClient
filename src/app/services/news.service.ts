c7d78a71d2faf9270d4d9a713b94dc9837351727
ericb <eric_bastarache@hotmail.com>
2017-02-17 02:43:18 -0500

    Updated news service

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
 import 'rxjs/Rx';
 
 @Injectable()
 export class NewsService {
   private baseUrl: string = 'https://newsapi.org/v1';
+  id: string;
+  private subscription: Subscription;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private router: Router, private activeRoute: ActivatedRoute) { }
 
   getSources () {
     let sources = this.http.get(`${this.baseUrl}/sources?lang=en`)
       .map((response: Response) => response.json());
     return sources;
+  }
+
+  getArticleList () {
+
   }
 }
