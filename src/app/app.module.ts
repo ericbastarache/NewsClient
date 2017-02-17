d0bbef7a1fe88d0437088428b202711b3804ec14
ericb <eric_bastarache@hotmail.com>
2017-02-17 13:24:59 -0500

    Removed obsolete article item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,10 @@ import { NewsListComponent } from './news-list/news-list.component';
 import { NewsService } from './services/news.service';
 import { NewsNavbarComponent } from './news-navbar/news-navbar.component';
 import { ArticleListComponent } from './article-list/article-list.component';
-import { ArticleItemComponent } from './article-item/article-item.component';
 
 const appRoutes: Routes = [
   // { path: 'article-list', component: ArticleListComponent },
   { path: 'article-list/:id', component: ArticleListComponent },
-  { path: 'article-item', component: ArticleItemComponent },
   { path: '**',
     redirectTo: '/',
     pathMatch: 'full'
@@ -29,8 +27,7 @@ const appRoutes: Routes = [
     AppComponent,
     NewsListComponent,
     NewsNavbarComponent,
-    ArticleListComponent,
-    ArticleItemComponent
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
