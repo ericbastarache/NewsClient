7b2dc28b7a36969dca25abb1fee8490b376f0923
ericb <eric_bastarache@hotmail.com>
2017-02-16 22:23:56 -0500

    Added source id for testing

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article-list',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: []
 })
 export class ArticleListComponent implements OnInit {
-
+  @Input() sourceID: number;
+  
   constructor() { }
 
   ngOnInit() {
